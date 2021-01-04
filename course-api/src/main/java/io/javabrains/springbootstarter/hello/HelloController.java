204b5e98278f694e60ba15e3592c0e3c7bc4b5fb
zaoad <zaoadcsedu22.23@gmail.com>
2021-01-04 15:52:28 +0600

    Add files via upload

---
+++ course-api/src/main/java/io/javabrains/springbootstarter/hello/HelloController.java
@@ -0,0 +1,13 @@
+package io.javabrains.springbootstarter.hello;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+@RestController
+public class HelloController {
+	@RequestMapping("/hello")
+	public String sayHi()
+	{
+		return "hello I am zaoad";
+	}
+	
+
+}
