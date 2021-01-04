332f68c14dfbfc1f66315521006eeb3b7316fabe
zaoad <zaoadcsedu22.23@gmail.com>
2021-01-04 15:54:50 +0600

    Add files via upload

---
+++ jpa-demo/src/test/java/zaoad/project/jpademo/JpaDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package zaoad.project.jpademo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
